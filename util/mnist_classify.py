1a797806720f7efc98b5382990f61889b095f473
QuangN313 <nh.quang313@gmail.com>
2018-12-08 02:03:54 +0700

    edit mnist code

--- util/mnist_classify.py
+++ util/mnist_classify.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     x_train = x_train.reshape(x_train.shape + (1,))
     x_test = x_test.reshape(x_test.shape + (1,))
 
-    clf = autokeras.image_supervised.ImageClassifier(verbose=True)
+    clf = autokeras.ImageClassifier(verbose=True)
     clf.fit(x_train, y_train, time_limit=12 * 60 * 60)
     clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
     y = clf.evaluate(x_test, y_test)
