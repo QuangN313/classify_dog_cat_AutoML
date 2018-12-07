8a2ec3d5e9a85c587fb99f46cf6a91fedfc85aec
QuangN313 <nh.quang313@gmail.com>
2018-12-08 02:52:56 +0700

    svae model

--- util/mnist_classify.py
+++ util/mnist_classify.py
@@ -1,13 +1,17 @@
 from keras.datasets import mnist
 import autokeras
+import os
 
 if __name__ == '__main__':
+    DIR_NAME = os.path.dirname(os.path.dirname(os.path.relpath(__file__)))
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train = x_train.reshape(x_train.shape + (1,))
     x_test = x_test.reshape(x_test.shape + (1,))
 
     clf = autokeras.ImageClassifier(verbose=True)
-    clf.fit(x_train, y_train, time_limit=5 * 60)
+    clf.fit(x_train, y_train, time_limit=30 * 60)
     clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
+    clf.load_searcher().load_best_model().produce_keras_model().save(os.join.path(DIR_NAME, 'util/tmp/model.h5')
     y = clf.evaluate(x_test, y_test)
-    print(y)
+    with open(os.path.join(DIR_NAME, 'util/report.txt'), 'a') as f:
+        f.writelines(f'Evaluation: {y}')
